82947063278037eec8f669062b91f6576a7da2de
Daniel Sebastian Nunes Cavalcanti <daniel.sebastian.nc@gmail.com>
2025-01-13 21:16:13 -0300

    first commit

---
+++ src/main/java/br/com/dev/danielsebastian/advanced_todo/model/Task.java
@@ -0,0 +1,92 @@
+package br.com.dev.danielsebastian.advanced_todo.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDate deadline;
+
+    @Column(nullable = true)
+    private LocalDate fineshedAt;
+
+    public Task(){
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public LocalDate getFineshedAt() {
+        return fineshedAt;
+    }
+
+    public void setFineshedAt(LocalDate fineshedAt) {
+        this.fineshedAt = fineshedAt;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Task task = (Task) object;
+        return Objects.equals(id, task.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "title='" + title + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
---
+++ src/test/java/br/com/dev/danielsebastian/advanced_todo/AdvancedTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.danielsebastian.advanced_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
